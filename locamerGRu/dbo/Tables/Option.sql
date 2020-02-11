460e59b51d335f80011e421eacca63de43057480
Gaëtan RUFFIER DES AIMES <ruffier.gaetan@btssio.local>
2020-02-11 15:20:03 +0100

    création enfin

---
+++ locamerGRu/dbo/Tables/Option.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Option] (
+    [idOption] INT           IDENTITY (1, 1) NOT NULL,
+    [libelle]  NVARCHAR (50) NOT NULL,
+    [prixJour] FLOAT (53)    NOT NULL,
+    PRIMARY KEY CLUSTERED ([idOption] ASC)
+);
+
---
+++ locamerGRu/dbo/Tables/Type.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Type] (
+    [idReduc]   INT           IDENTITY (1, 1) NOT NULL,
+    [libelle]   NVARCHAR (50) NOT NULL,
+    [coefReduc] FLOAT (53)    NOT NULL,
+    PRIMARY KEY CLUSTERED ([idReduc] ASC)
+);
+
